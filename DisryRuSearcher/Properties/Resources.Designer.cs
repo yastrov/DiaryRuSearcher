030a8e4002aa48b12a8555426fbc9a073d7a37a0
Yuriy Astrov <yuriastrov@gmail.com>
2015-01-27 19:51:53 +0300

    Fix Assembly Info for project and fix output filename for solution.

--- DisryRuSearcher/Properties/Resources.Designer.cs
+++ DisryRuSearcher/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DisryRuSearcher.Properties {
+namespace DiaryRuSearcher.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DisryRuSearcher.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DisryRuSearcher.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DiaryRuSearcher.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- DisryRuSearcher/Properties/Settings.Designer.cs
+++ DisryRuSearcher/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DisryRuSearcher.Properties {
+namespace DiaryRuSearcher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

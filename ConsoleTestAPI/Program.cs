1def74fead03f4441a3fba5a269eefb5ebf87a9f
Yuriy Astrov <yuriastrov@gmail.com>
2015-02-25 19:40:03 +0300

    ConsoleTest deleted.

--- ConsoleTestAPI/Program.cs
+++ ConsoleTestAPI/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using DiaryAPI;
+using DiaryAPI.JSONResponseClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DiaryAPI;
-using DiaryAPI.JSONResponseClasses;
+
 
 namespace ConsoleTestAPI
 {

8089f75e18a4e05b56901f4bc96ad9b97ace3c45
Yuriy Astrov <yuriastrov@gmail.com>
2015-03-13 20:50:11 +0300

    DiaryApiClient - added user.get method and JSON structs

---
+++ DiaryAPIClient/DiaryAPIClientUser.cs
@@ -0,0 +1,48 @@
+﻿using DiaryAPI.JSONResponseClasses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryAPI
+{
+    public partial class DiaryAPIClient
+    {
+        public UserUnit UserGet(string userid)
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("method", "user.get");
+            nvc.Add("sid", _sid);
+            if (!String.IsNullOrEmpty(userid))
+                nvc.Add("userid", userid);
+            var url = NameValueCollectionToUrlStringUrlEncoding(nvc);
+            using (HttpWebResponse response = this._Request(url, "GET", null))
+            {
+                var r = GetObjectFromJson<UserGetJSONResponse>(response);
+                if (r.CheckForError()) throw new DiaryAPIClientException(r.Error);
+                return r.User;
+            }
+        }
+
+        public async Task<UserUnit> UserGetAsync (string userid)
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("method", "user.get");
+            nvc.Add("sid", _sid);
+            if (!String.IsNullOrEmpty(userid))
+                nvc.Add("userid", userid);
+            var url = NameValueCollectionToUrlStringUrlEncoding(nvc);
+            using (HttpWebResponse response = await this._RequestAsync(url, "GET", null))
+            {
+                var r = GetObjectFromJson<UserGetJSONResponse>(response);
+                if (r.CheckForError()) throw new DiaryAPIClientException(r.Error);
+                return r.User;
+            }
+        }
+    }
+}
---
+++ DiaryAPIClient/JSONResponseClasses/UserGetJSONResponse.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryAPI.JSONResponseClasses
+{
+    [JsonObject]
+    public class UserGetJSONResponse : IErrorChecked
+    {
+        public string Result { get; set; }
+        [JsonProperty("user")]
+        public UserUnit User { get; set; }
+
+        public string Error { get; set; }
+
+        public Boolean CheckForError()
+        {
+            return !String.IsNullOrEmpty(this.Error);
+        }
+        public String GetErrorAsString()
+        {
+            return this.Error;
+        }
+    }
+
+    public class UserUnit
+    {
+        public string journal { get; set; }
+        public string about { get; set; }
+        public string sex { get; set; }
+        public string joindate { get; set; }
+        public string tw { get; set; }
+        public string birthday { get; set; }
+        public string country { get; set; }
+        [JsonProperty("yo")]
+        public string AgeAsStr { get; set; }
+        public string journal_title { get; set; }
+        [SQLite.PrimaryKey]
+        public string userid { get; set; }
+        [SQLite.Ignore]
+        public Dictionary<int, String> Readers2 { get; set; }
+        public string city { get; set; }
+        public string[] readers { get; set; }
+        public string avatar { get; set; }
+        public string shortname { get; set; }
+        [JsonProperty("community.member")]
+        public string[] communitymember { get; set; }
+        [SQLite.Ignore]
+        [JsonProperty("community.member2")]
+        public Dictionary<int, String> communitymember2 { get; set; }
+        public string[] fav { get; set; }
+        public int readers_count { get; set; }
+        [JsonProperty("community.member_count")]
+        public int communitymember_count { get; set; }
+        public string username { get; set; }
+        public string joindate2 { get; set; }
+        public string locked { get; set; }
+        public int favs_count { get; set; }
+        [SQLite.Ignore]
+        public Dictionary<int, String> favs2 { get; set; }
+        public string education { get; set; }
+        public string birthday2 { get; set; }
+        [SQLite.Ignore]
+        public Dictionary<int, String> interest { get; set; }
+        public string region { get; set; }
+
+        [SQLite.Ignore]
+        public string Url { get { return this.MakeUrl(); } }
+        public string MakeUrl()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("http://www.diary.ru/member/?").Append(this.userid);
+            return sb.ToString();
+        }
+    }
+
+
+}

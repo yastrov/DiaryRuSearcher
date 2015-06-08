3d55bc3f98bea0c14c25d222bd2d39d413732dbe
Yuriy Astrov <yuriastrov@gmail.com>
2015-06-08 19:51:22 +0300

    DiaryAPIClient: Now support for notifications.

---
+++ DiaryAPIClient/DiaryAPIClientNotifications.cs
@@ -0,0 +1,42 @@
+﻿using DiaryAPI.JSONResponseClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryAPI
+{
+    public partial class DiaryAPIClient
+    {
+        public NotificationsUnit NotificationsGet()
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("method", "notification.get");
+            nvc.Add("sid", SID);
+            var url = NameValueCollectionToUrlStringUrlEncoding(nvc);
+            using (HttpWebResponse response = this._Request(url, "GET", null))
+            {
+                var r = GetObjectFromJson<NotificationsUnit>(response);
+                if (r.CheckForError()) throw new DiaryAPIClientException(r.Error);
+                return r;
+            }
+        }
+
+        public async Task<NotificationsUnit> NotificationsGetAsync()
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            nvc.Add("method", "notification.get");
+            nvc.Add("sid", SID);
+            var url = NameValueCollectionToUrlStringUrlEncoding(nvc);
+            using (HttpWebResponse response = await this._RequestAsync(url, "GET", null))
+            {
+                var r = GetObjectFromJson<NotificationsUnit>(response);
+                if (r.CheckForError()) throw new DiaryAPIClientException(r.Error);
+                return r;
+            }
+        }
+    }
+}
---
+++ DiaryAPIClient/JSONResponseClasses/NotificationsJSONResponse.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryAPI.JSONResponseClasses
+{
+    public class NotificationsUnit : IErrorChecked
+    {
+        [JsonProperty("comments_count")]
+        public int CommentsCount { get; set; }
+        [JsonProperty("discuss_count")]
+        public int DiscussCount {get; set; }
+        [JsonProperty("umail_count")]
+        public int UmailsCount { get; set; }
+
+        [JsonProperty("comments")]
+        public Dictionary<string, CommentNotifyInfo> Comments { get; set; }
+        [JsonProperty("discuss")]
+        public DiscussNotifyInfo[] Discuss { get; set; }
+        [JsonProperty("umail")]
+        public Dictionary<string, UmailNotifyInfo> Umails { get; set; }
+        [JsonProperty("result")]
+        public string Result { get; set; }
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        public Boolean CheckForError()
+        {
+            return !String.IsNullOrEmpty(this.Error);
+        }
+        public String GetErrorAsString()
+        {
+            return this.Error;
+        }
+
+        public IEnumerable<CommentNotifyInfo> GetComments()
+        {
+            return Comments.Values.ToList();
+        }
+        public IEnumerable<UmailNotifyInfo> GetUmails()
+        {
+            return Umails.Values.ToList();
+        }
+        public IEnumerable<DiscussNotifyInfo> GetDiscuss()
+        {
+            return Discuss.ToList();
+        }
+    }
+
+    public class CommentNotifyInfo
+    {
+        [JsonProperty("commentid")]
+        public string CommentId { get; set; }
+        [JsonProperty("message_txt")]
+        public string MessageTxt { get; set; }
+        [JsonProperty("postid")]
+        public string PostId { get; set; }
+    }
+
+    public class DiscussNotifyInfo
+    {
+        [JsonProperty("count")]
+        public int Count { get; set; }
+        [JsonProperty("dateline")]
+        public string Dateline { get; set; }
+        [JsonProperty("journal_name")]
+        public string JournalName { get; set; }
+        [JsonProperty("juserid")]
+        public string JuserId { get; set; }
+        [JsonProperty("postid")]
+        public string PostId { get; set; }
+    }
+
+    public class UmailNotifyInfo
+    {
+        [JsonProperty("dateline")]
+        public string Dateline { get; set; }
+        [JsonProperty("message_txt")]
+        public string MessageTxt { get; set; }
+        [JsonProperty("folder")]
+        public string FolderId { get; set; }
+        [JsonProperty("from_userid")]
+        public string FromUserid { get; set; }
+        [JsonProperty("from_username")]
+        public string FromUsername { get; set; }
+        [JsonProperty("title")]
+        public string  Title { get; set; }
+        [JsonProperty("umailid")]
+        public string UmailId { get; set; }
+    }
+}
